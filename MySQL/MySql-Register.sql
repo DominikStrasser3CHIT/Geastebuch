211eb37fa8ed2bf7fc0eeafea065d3c3110d45a5
DominikStrasser3CHIT <dominikstrasser@Dominiks-MacBook-Pro-2.local>
2015-01-13 14:33:43 +0100

    MySQL Datenbank korrekturen

--- MySQL/MySql-Register.sql
+++ MySQL/MySql-Register.sql
@@ -1,11 +1,12 @@
-DROP DATABASE IF EXISTS Gaestebuch;
-CREATE DATABASE Gaestebuch; 
-USE Gaestebuch;
-DROP TABLE IF EXISTS Eintrag;
-CREATE TABLE Eintrag(
+DROP DATABASE IF EXISTS gaestebuch;
+CREATE DATABASE gaestebuch; 
+USE gaestebuch;
+DROP TABLE IF EXISTS eintrag;
+CREATE TABLE eintrag(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     datum DATE,
-    uberschrift VARCHAR(40) NOT NULL,
+    ueberschrift VARCHAR(40) NOT NULL,
     text VARCHAR(125) NOT NULL
 ) ENGINE = InnoDB;
+
